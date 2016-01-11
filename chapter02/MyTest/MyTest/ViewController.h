3f7a6efccbaaaf9c631702f39a8d826b3ab37d5f
saintpablo94 <saint.pablo94@gmail.com>
2016-01-11 22:41:56 +0900

    68P imageView 다시확인하기

--- chapter02/MyTest/MyTest/ViewController.h
+++ chapter02/MyTest/MyTest/ViewController.h
@@ -9,6 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UISwitch *switchButton;
+- (IBAction)switchTouch:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+- (IBAction)inputTextToTextView:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 - (IBAction)buttonTouch:(id)sender;
--- chapter02/MyTest/MyTest/ViewController.m
+++ chapter02/MyTest/MyTest/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [_imageView setImage:[UIImage imageWithContentsOfFile:
+                [[NSBundle mainBundle] pathForResource:@"Amazonprime" ofType:@"jpg"]]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,5 +30,15 @@
     NSString *str = [_textField text];
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:str]];
     [_webView loadRequest:request];
+}
+- (IBAction)inputTextToTextView:(id)sender {
+    [_textView setText:[_textField text]];
+}
+- (IBAction)switchTouch:(id)sender {
+    if([_switchButton isOn]){
+        [_imageView setAlpha:1.0f];
+    }else{
+        [_imageView setAlpha:0.0f];
+    }
 }
 @end
